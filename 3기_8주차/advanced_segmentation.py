582699729ec2df2daaf3ac92d25081060e6c025f
Ryuchanghoon <fbckdgns3@naver.com>
2024-12-19 13:02:39 +0900

    8주차 이미지 처리 수업 자료

---
+++ 3기_8주차/advanced_segmentation.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+import torch
+# pip install torch torchvision
+# pip install git+https://github.com/facebookresearch/detectron2.git
+
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml"))
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+predictor = DefaultPredictor(cfg)
+
+image = cv2.imread("snow_example.png")
+
+
+outputs = predictor(image)
+segmentation = outputs["sem_seg"].argmax(dim=0).to("cpu").numpy()
+
+
+metadata = MetadataCatalog.get(cfg.DATASETS.TRAIN[0])
+
+snow_class_index = metadata.stuff_classes.index("snow")
+
+snow_mask = (segmentation == snow_class_index).astype(np.uint8)
+
+colored_seg = Visualizer(image[:, :, ::-1], metadata, scale=1.2)
+result = colored_seg.draw_binary_mask(snow_mask, color=[1.0, 1.0, 1.0], alpha=0.5)
+
+
+cv2.imshow('source image', image)
+cv2.imshow('semantic segmentation - just snow', result.get_image()[:, :, ::-1])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
